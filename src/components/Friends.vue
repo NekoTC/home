6d2fb3ebf67e3aa375efa6e3467a8430d4fc9748
NekoTC <xtwg17192522@outlook.com>
2024-03-23 23:59:48 +0800

    Update Info

--- src/components/Friends.vue
+++ src/components/Friends.vue
@@ -5,7 +5,7 @@
     </div>
     <!-- 友链 -->
     <div class="clear" id="links">
-        <div class="link-all">
+        <div class="item">
           <div v-if="loading">加载中...</div>
           <div v-else>
             <div v-for="link in links" :key="link.id" class="item">
