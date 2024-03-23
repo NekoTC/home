2f29ff70f3c3e0d3efb0ef3b4c0c8f55c7a40c68
NekoTC <xtwg17192522@outlook.com>
2024-03-23 23:29:14 +0800

    Update Info

--- src/components/Friends.vue
+++ src/components/Friends.vue
@@ -12,7 +12,7 @@
             <div v-for="link in links" :key="link.id" class="item">
               <a :href="link.url" target="_blank">
                 <div class="avatar"><img :src="link.avatar" width="64" height="64"></div>
-                <div class="inner">
+                <div class="item">
                   <h5 class="name">{{ link.name }}</h5>
                   <p>{{ link.description }}</p>
                 </div>
