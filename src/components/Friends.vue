9eb5e172d4d8ba017df84d5e433f7ec3f907339c
NekoTC <xtwg17192522@outlook.com>
2024-03-24 00:21:36 +0800

    Update Info

--- src/components/Friends.vue
+++ src/components/Friends.vue
@@ -9,11 +9,11 @@
         <div class="link-all">
           <div v-if="loading">加载中...</div>
           <el-row class="link-all" :gutter="20">
-            <el-col v-for="(item, index) in site" :span="8" :key="item">
+            <el-col v-for="(link, index) in site" :span="8" :key="link.id">
               <div
                 class="item cards"
-                :style="index < 3 ? 'margin-bottom: 20px' : null"
-                @click="link.url"
+                :style="[index < 3 ? { marginBottom: '20px' } : null]"
+                @click="visitLink(link.url)"
               >
                 <Icon size="32">
                   <component :is="link.avatar" />
@@ -31,15 +31,34 @@
 <script>
 import axios from 'axios';
 
-axios.get('https://mxapi.nekotc.cn/api/v2/links/all')
-  .then(response => {
-    // 在这里处理返回的 JSON 数据
-    console.log(response.link);
-  })
-  .catch(error => {
-    console.error('发生错误:', error);
-  });
-
+export default {
+  data() {
+    return {
+      loading: true,
+      site: [] // 存储友链数据
+    };
+  },
+  created() {
+    this.fetchLinks();
+  },
+  methods: {
+    fetchLinks() {
+      axios.get('https://mxapi.nekotc.cn/api/v2/links/all')
+        .then(response => {
+          // 在这里处理返回的 JSON 数据
+          this.site = response.data.data; // 将获取到的数据保存到组件的数据中
+          this.loading = false; // 设置加载状态为完成
+        })
+        .catch(error => {
+          console.error('发生错误:', error);
+        });
+    },
+    visitLink(url) {
+      // 处理点击链接事件
+      window.open(url, '_blank');
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
