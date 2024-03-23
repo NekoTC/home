03a0339d50b2797eae9c750184458880dbccc43c
NekoTC <xtwg17192522@outlook.com>
2024-03-23 22:08:28 +0800

    Update Info

--- src/components/Friends.vue
+++ src/components/Friends.vue
@@ -9,9 +9,9 @@
             <div v-else>
               <div v-for="link in links" :key="link.id" class="item">
                 <a :href="link.url" target="_blank">
-                  <div class="avatar"><img :src="link.avatar" width="64" height="64"></div>
-                  <div class="inner" width="256" height="128">
-                    <h5 class="name">{{ link.name }}</h5>
+                  <div class="avatar centered"><img :src="link.avatar" width="64" height="64"></div>
+                  <div class="inner">
+                    <h5 class="name centered">{{ link.name }}</h5>
                     <p>{{ link.description }}</p>
                   </div>
                 </a>
@@ -60,53 +60,4 @@ export default {
 /* 引入外部 CSS 文件 */
 @import url('src/components/css/h.9c69ed6c.css');
 @import url('src/components/css/nekotora.99cf6f8c.css');
-
-/* 这里可以继续添加你的样式 */
-.links {
-  .line {
-    margin: 2rem 0.25rem 1rem;
-    font-size: 1.1rem;
-    display: flex;
-    align-items: center;
-    animation: fade 0.5s;
-    .title {
-      margin-left: 8px;
-      font-size: 1.15rem;
-      text-shadow: 0 0 5px #00000050;
-    }
-  }
-  .link-all {
-    height: 220px;
-    .item {
-      height: 100px;
-      width: 100%;
-      display: flex;
-      align-items: center;
-      flex-direction: row;
-      justify-content: center;
-      padding: 0 10px;
-      animation: fade 0.5s;
-
-      &:hover {
-        transform: scale(1.02);
-        background: rgb(0 0 0 / 40%);
-        transition: 0.3s;
-      }
-
-      &:active {
-        transform: scale(1);
-      }
-
-      .avatar img {
-        width: 64px;
-        height: 64px;
-      }
-
-      .name {
-        font-size: 1.1rem;
-        margin-left: 8px;
-      }
-    }
-  }
-}
 </style>
