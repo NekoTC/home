7d3f6bcd5dfc8e764aa571025d463f3fe3d75db8
NekoTC <xtwg17192522@outlook.com>
2024-03-23 22:49:43 +0800

    Update Info

--- src/components/Friends.vue
+++ src/components/Friends.vue
@@ -1,30 +1,26 @@
 <template>
   <div class="gate ch">
     <h2>我的<span>好朋友</span>们</h2>
-      <!-- 友链 -->
-      <div class="clear" id="links">
-        <div class="links">
-          <div class="link-all">
-            <div v-if="loading">加载中...</div>
-            <div v-else>
-              <div v-for="link in links" :key="link.id" class="item">
-                <a :href="link.url" target="_blank">
-                  <div
-                    class="item cards"
-                    :style="index < 3 ? 'margin-bottom: 20px' : null"
-                    @click="jumpLink(item)"
-                  >
-                    <div class="avatar centered"><img :src="link.avatar" width="64" height="64"></div>
-                    <span class="name text-hidden">{{ link.name }}</span>
-                    <p>{{ link.description }}</p>
-                  </div>
-                </a>
-              </div>
+    <!-- 友链 -->
+    <div class="clear" id="links">
+      <div class="links">
+        <div class="link-all">
+          <div v-if="loading">加载中...</div>
+          <div v-else>
+            <div v-for="link in links" :key="link.id" class="item">
+              <a :href="link.url" target="_blank">
+                <div class="avatar"><img :src="link.avatar" width="64" height="64"></div>
+                <div class="inner">
+                  <h5 class="name">{{ link.name }}</h5>
+                  <p>{{ link.description }}</p>
+                </div>
+              </a>
             </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -61,99 +57,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.links {
-  .line {
-    margin: 2rem 0.25rem 1rem;
-    font-size: 1.1rem;
-    display: flex;
-    align-items: center;
-    animation: fade 0.5s;
-    .title {
-      margin-left: 8px;
-      font-size: 1.15rem;
-      text-shadow: 0 0 5px #00000050;
-    }
-  }
-  .swiper {
-    left: -10px;
-    width: calc(100% + 20px);
-    padding: 5px 10px 0;
-    z-index: 0;
-    .swiper-slide {
-      height: 100%;
-    }
-    .swiper-pagination {
-      position: static;
-      margin-top: 4px;
-      :deep(.swiper-pagination-bullet) {
-        background-color: #fff;
-        width: 18px;
-        height: 4px;
-        border-radius: 4px;
-        transition: opacity 0.3s;
-        &:hover {
-          opacity: 1;
-        }
-      }
-    }
-  }
-  .link-all {
-    height: 220px;
-    .item {
-      height: 100px;
-      width: 100%;
-      display: flex;
-      align-items: center;
-      flex-direction: row;
-      justify-content: center;
-      padding: 0 10px;
-      animation: fade 0.5s;
-
-      &:hover {
-        transform: scale(1.02);
-        background: rgb(0 0 0 / 40%);
-        transition: 0.3s;
-      }
-
-      &:active {
-        transform: scale(1);
-      }
-
-      .name {
-        font-size: 1.1rem;
-        margin-left: 8px;
-      }
-      @media (min-width: 720px) and (max-width: 820px) {
-        .name {
-          display: none;
-        }
-      }
-      @media (max-width: 720px) {
-        height: 80px;
-      }
-      @media (max-width: 460px) {
-        flex-direction: column;
-        .name {
-          font-size: 1rem;
-          margin-left: 0;
-          margin-top: 8px;
-        }
-      }
-    }
-    @media (max-width: 720px) {
-      height: 180px;
-    }
-  }
-}
-.gate .links .item .avatar {
-  float: left;
-  height: 60px;
-  line-height: 60px;
-  width: 60px;
-  border-radius: 100%;
-  text-align: center;
-  margin-right: 15px;
-  background-color: #353535;
-  overflow: hidden
-}
+@import url('src/components/css/h.9c69ed6c.css');
+@import url('src/components/css/nekotora.99cf6f8c.css');
 </style>
