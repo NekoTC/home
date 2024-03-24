a35a78be53db8b2ef9e627dd0d9d51848ee6ea61
NekoTC <xtwg17192522@outlook.com>
2024-03-24 10:28:20 +0800

    Update Info

--- src/components/Friends.vue
+++ src/components/Friends.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="clear" id="links">
-    <div v-if="loading">加载中...</div>
-    <div v-else>
-      <div v-for="link in links" :key="link.id" class="item">
-        <a :href="link.url" target="_blank">
-          <div class="avatar"><img :src="link.avatar"></div>
-          <div class="inner">
-            <h5>{{ link.name }}</h5>
-            <p>{{ link.description }}</p>
-          </div>
-        </a>
+  <div class="gate ch">
+    <div class="container links">
+      <h2 class="chtitle">我的
+        <span>好朋友</span>们</h2>
+    <div class="clear" id="links">
+      <div v-if="loading">加载中...</div>
+      <div v-else>
+        <div v-for="link in links" :key="link.id" class="item">
+          <a :href="link.url" target="_blank">
+            <div class="avatar"><img :src="link.avatar"></div>
+            <div class="inner">
+             <h5>{{ link.name }}</h5>
+             <p>{{ link.description }}</p>
+           </div>
+          </a>
+        </div>
       </div>
+    </div>
     </div>
   </div>
 </template>
